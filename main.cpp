c96dbded6e0fa543e75dd2e1dd0be22a73a72a6d
Gabriela Lopez Diaz <lopezgaby811@gmail.com>
2021-05-06 20:22:49 -0700

    fsdfsdf

--- main.cpp
+++ main.cpp
@@ -37,20 +37,20 @@ int menuOption()
 {
 	system("cls");
 	cout << '\n';
-	cout << '\t' << char(201) << string(43, char(205)) << char(187) << '\n';
-	cout << '\t' << char(186) << "      \t    CMPR131: Chapter 9              " << char(186) << '\n';
-	cout << '\t' << char(186) << "      \t\t  Made by:                  " << char(186) << '\n';
-	cout << '\t' << char(186) << "   - Gabriela Lopez\t- Andrew Thompson   " << char(186) << '\n';
-	cout << '\t' << char(186) << "   - Lincoln Nguyen\t- Long Duong        " << char(186) << '\n';
-	cout << '\t' << char(186) << "   - Christopher Gomez\t- Gia Minh Hoang    " << char(186) << '\n';
-	cout << '\t' << char(186) << "   - Duyen Tran\t- Thien Vu          " << char(186) << '\n';
-	cout << "\t" << char(204) << string(43, char(205)) << char(185) << "\n";
-	cout << "\t" << char(186) << "   1: binary_tree_node                     " << char(186) << "\n";
-	cout << "\t" << char(186) << "   2: BinaryTree                           " << char(186) << "\n";
-	cout << "\t" << char(186) << "   3: Animal Guessing Game                 " << char(186) << "\n";
-	cout << "\t" << char(199) << string(43, char(196)) << char(182) << "\n";
-	cout << "\t" << char(186) << "   0: Exit                                 " << char(186) << "\n";
-	cout << "\t" << char(200) << string(43, char(205)) << char(188) << "\n";
+	cout << '\t' << char(201) << string(50, char(205)) << char(187) << '\n';
+	cout << '\t' << char(186) << "      \t        CMPR131: Chapter 10                " << char(186) << '\n';
+	cout << '\t' << char(186) << "      \t\t      Made by:                     " << char(186) << '\n';
+	cout << '\t' << char(186) << "      - Gabriela Lopez\t    - Andrew Thompson      " << char(186) << '\n';
+	cout << '\t' << char(186) << "      - Lincoln Nguyen\t    - Long Duong           " << char(186) << '\n';
+	cout << '\t' << char(186) << "      - Christopher Gomez  - Gia Minh Hoang       " << char(186) << '\n';
+	cout << '\t' << char(186) << "      - Duyen Tran\t    - Thien Vu             " << char(186) << '\n';
+	cout << "\t" << char(204) << string(50, char(205)) << char(185) << "\n";
+	cout << "\t" << char(186) << "   1: Bonsai Tree of strings                      " << char(186) << "\n";
+	cout << "\t" << char(186) << "   2: Template BinaryTree container of integers   " << char(186) << "\n";
+	cout << "\t" << char(186) << "   3: Animal Guessing Game                        " << char(186) << "\n";
+	cout << "\t" << char(199) << string(50, char(196)) << char(182) << "\n";
+	cout << "\t" << char(186) << "   0: Exit                                        " << char(186) << "\n";
+	cout << "\t" << char(200) << string(50, char(205)) << char(188) << "\n";
 
 	int option = inputInteger("\t    Option: ", 0, 3);
 	cin.clear();
